b5c03ec3e9e48614fc49099c2f0dcc5032f18e7d
shwetavashist <shwetasharma498@gmail.com>
2017-11-02 13:38:30 +0530

    my commit

--- targertParent/targetParent.js
+++ targertParent/targetParent.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-        $("#target1").css("color", "red");
-        $("#target1").prop("disabled", true);
-        $("#target4").remove();
-        $("#target2").appendTo("#right-well");
-        $("#target5").clone().appendTo("#left-well");    
-        $("#target1").parent().css("background-color", "red");
+$(document).ready(function() {   
+        $("#target1").parent().css("background-color","red");
   });
