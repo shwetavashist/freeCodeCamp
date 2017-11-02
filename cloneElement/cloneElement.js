4e7f7011539628c7e5ad4747b9a37d55968b78c1
shwetavashist <shwetasharma498@gmail.com>
2017-11-02 12:27:15 +0530

    my first commit

---
+++ cloneElement/cloneElement.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+$("#target5").clone().appendTo("#left-well");
+  });
